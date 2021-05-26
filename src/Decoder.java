2f883743f0772147a4a4033dcf90f265d946abaa
Miguel Puerta <63917673+migtarx@users.noreply.github.com>
2021-05-26 23:07:47 +0200

    Code optimization
    Replaced secondary for loop with a simple indexOf method.

--- src/Decoder.java
+++ src/Decoder.java
@@ -8,24 +8,17 @@ public class Decoder {
 		String codedMessage = "";
 		
 		for (int i = 0; i < message.length(); i++) {
-			for (int j = 0; j < alphabet.length(); j++) {
-				if (message.charAt(i) == alphabet.charAt(j)) {
-					codedMessage = codedMessage + rot13Alphabet.charAt(j);
-				}
-			}
+			codedMessage = codedMessage + rot13Alphabet.charAt(alphabet.indexOf(message.charAt(i)));
+			
 		}
-		return codedMessage;		
+		return codedMessage;	
 	}
 	
 	public String decrypt(String codedMessage) {
 		String decodedMessage = "";
 		
 		for (int i = 0; i < codedMessage.length(); i++) {
-			for (int j = 0; j < rot13Alphabet.length(); j++) {
-				if (codedMessage.charAt(i) == rot13Alphabet.charAt(j)) {
-					decodedMessage = decodedMessage + alphabet.charAt(j);
-				}
-			}
+			decodedMessage = decodedMessage + alphabet.charAt(rot13Alphabet.indexOf(message.charAt(i)));
 		}	
 		return decodedMessage;	
 	}
